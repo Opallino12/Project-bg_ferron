a3dc2b54bc37ff60d4fbfe426e4b4215d6000961
Ahmad Naufal <145427048+Opallino12@users.noreply.github.com>
2024-08-28 00:14:01 +0700

    Update editPost

--- main.js
+++ main.js
@@ -20,33 +20,70 @@ let formValidation = () => {
         console.log("successs");
         msg.innerHTML = "";
         acceptData();
+
     }
 };
 
-let data = {};
+let data = [];
 
 let acceptData = () => {
-    data["nama"] = nama.value;
-    data["suku"] = suku.value;
-    data["tgl"] = tgl.value;
-    data["tmpt"] = tmpt.value;
+    data.push({
+        namas: nama.value,
+        sukus: suku.value,
+        tmpts: tmpt.value,
+        tgls: tgl.value,
+    });
+    localStorage.setItem("data", JSON.stringify(data));
+    
     console.log(data);
     createPost();
 };
 
 let createPost = () =>{
-    posts.innerHTML +=`
-    <tr>
-        <td>${data.nama}</td>
-        <td>${data.suku}</td>
-        <td>${data.tmpt},${data.tgl}</td>
-        <td><i onclick="editPost(this)"></i></td>
-        <td><i onclick="deletePost(this)"></i></td>
-    </tr>`;
+    data.map((x) => {
+        return (posts.innerHTML +=`
+            <tr>
+                <td>${x.namas}</td>
+                <td>${x.sukus}</td>
+                <td>${x.tmpts}</td>
+                <td>${x.tgls}</td>
+                <td><button onclick="editPost(this)">Edit</button></td>
+                <td><button onclick="deletePost(this)">Hapus</button></td>
+            </tr>`);
+    });
+    
+    resetForm();
+};
+
+let resetForm = () => {
     nama.value = "";
     suku.value = "";
     tgl.value = "";
     tmpt.value = "";
 };
 
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
 
+    data.splice(e.parentElement.parentElement.id, 1);
+
+    localStorage.setItem("data", JSON.stringify(data));
+
+    console.log(data);
+};
+let editPost = (e) => {
+    let selectedPost = e.parentElement.parentElement;
+
+    nama.value = selectedPost.children[0].innerHTML;
+    suku.value = selectedPost.children[1].innerHTML;
+    tmpt.value = selectedPost.children[2].innerHTML;
+    tgl.value = selectedPost.children[3].innerHTML;
+    
+    deletePost(e);
+};
+
+(() => {
+    data = JSON.parse(localStorage.getItem("data")) || [];
+    console.log(data);
+    createPost();
+})();
